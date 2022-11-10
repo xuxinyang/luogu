e487b97e28b5e6e7d6070a06e0cad95ce9efb428
yangxuxin <18729346082@163.com>
2022-11-10 18:47:14 +0800

    fixed P1012

---
+++ 普及/P1012.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+string s[21];int n;
+bool cmp(const string &a,const string &b) { // &表示引用
+    return (a+b > b+a);
+}
+int main(void) {
+    cin >> n;
+    for(int i=1;i<=n;++i) cin >> s[i];
+    sort(s+1,s+n+1,cmp);
+    for (int i=1;i<=n;++i) cout << s[i];
+    return 0;
+}
