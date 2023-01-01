792e49558cb1ed01cb478a9d5429139731783da6
xuxinyang <18729346082@163.com>
2023-01-01 14:58:39 +0800

    update P2638, subtask1 is wa

--- 普及提高/P2638.cpp
+++ 普及提高/P2638.cpp
@@ -1,22 +1,47 @@
-#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
 using namespace std;
-#define ull unsigned long long
 int n, a, b;
-ull calc(int n, int r)
+struct Num
 {
-    ull ans = 1;
-    for (int i = 1; i <= r; ++i)
+    int l, a[30];
+} ans, dp[55][55][55];
+Num operator+(Num x, Num y)
+{
+    Num ret;
+    ret.l = max(x.l, y.l);
+    memset(ret.a, 0, sizeof(ret.a));
+    // 一定要清零！！！
+    for (int i = 1; i <= ret.l; i++)
     {
-        ans *= n - i + 1;
-        ans /= i;
+        ret.a[i] += x.a[i] + y.a[i];
+        ret.a[i + 1] += ret.a[i] / 10;
+        ret.a[i] %= 10;
     }
-    return ans;
+    if (ret.a[ret.l + 1])
+        ret.l++;
+    return ret;
+}
+void print(Num x)
+{
+    for (int i = x.l; i >= 1; i--)
+        printf("%d", x.a[i]);
+    printf("\n");
 }
 int main()
 {
-    cin >> n >> a >> b;
-    ull ans = calc(a+n, n) * calc(b+n, n);
-    if (ans == 2) ans = 1;
-    cout << ans << endl;
+    scanf("%d%d%d", &n, &a, &b);
+    dp[0][0][0].l = dp[0][0][0].a[1] = 1;
+    for (int i = 1; i <= n; i++)
+        for (int j = 0; j <= a; j++)
+            for (int k = 0; k <= b; k++)
+                for (int s1 = 0; s1 <= j; s1++)
+                    for (int s2 = 0; s2 <= k; s2++)
+                        dp[i][j][k] = dp[i][j][k] + dp[i - 1][s1][s2];
+    for (int i = 0; i <= a; i++)
+        for (int j = 0; j <= b; j++)
+            ans = ans + dp[n][i][j];
+    print(ans);
     return 0;
 }
