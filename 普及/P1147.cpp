6114326ed533b51a4146c4dfcbd1e09b4bcee9e8
xuxinyang <18729346082@163.com>
2023-01-01 15:21:48 +0800

    fixed P1147

---
+++ 普及/P1147.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int m;
+int main()
+{
+    cin >> m;
+    for (int k1 = sqrt(2 * m); k1 > 1; k1--)
+    {
+        if (2 * m % k1 == 0 && (k1 + 2 * m / k1) % 2)
+        {
+            int k2 = 2 * m / k1;
+            cout << (k2-k1+1)/2 << " " << (k1+k2-1)/2 << "\n";
+        }
+    }
+    return 0;
+}
