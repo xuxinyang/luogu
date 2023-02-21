1aedeae2d2f2aa3d616827e84526490c51c1d6fc
xuxinyang <18729346082@163.com>
2023-02-21 17:49:47 +0800

    fixed P1522

---
+++ PREANDADVANCE/P1522.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn = 155;
+const double inf = 1e20;
+struct Node
+{
+    int x;
+    int y;
+};
+Node nodes[maxn];
+int n, f[maxn];
+double dist[maxn], w[maxn][maxn], ans[maxn], maxx, minx = inf;
+string s;
+double distance(int x1, int y1, int x2, int y2)
+{
+    return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+}
+void dfs(int i, int id)
+{
+    f[i] = id;
+    for (int j = 1; j <= n; j++)
+        if (!f[j] && w[i][j] < inf) dfs(j, id);
+}
+int main()
+{
+    cin >> n;
+    for (int i = 1; i <= n; i++) cin >> nodes[i].x >> nodes[i].y;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> s;
+        s = " " + s;
+        for (int j = 1; j < s.length(); j++)
+        {
+            if (s[j] == '1' || i == j)
+                w[i][j] = distance(nodes[i].x, nodes[i].y, nodes[j].x, nodes[j].y);
+            else
+                w[i][j] = inf;
+        }
+    }
+    int id = 1;
+    for (int i = 1; i <= n; i++)
+        if (!f[i]) dfs(i, id++);
+    for (int k = 1; k <= n; k++)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (w[i][j] > w[i][k] + w[k][j])
+                    w[i][j] = w[i][k] + w[k][j];
+            }
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+            if (w[i][j] < inf) ans[i] = max(ans[i], w[i][j]);
+        dist[f[i]] = max(dist[f[i]], ans[i]);
+    }
+    for (int i = 1; i <= n; i++)
+        for (int j = i + 1; j <= n; j++)
+        {
+            if (f[i] != f[j])
+            {
+                maxx = max(max(dist[f[i]], dist[f[j]]), 
+                ans[i] + ans[j] + distance(nodes[i].x, nodes[i].y, nodes[j].x, nodes[j].y));
+                minx = min(minx, maxx);
+            }
+        }
+    cout << fixed << setprecision(6) << minx << "\n";
+    return 0;
+}
