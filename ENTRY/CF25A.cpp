093016b4cc86731677a8fd70bf0e014f8a2b9f86
xuxinyang <18729346082@163.com>
2023-02-23 11:32:29 +0800

    fixed CF25A

--- ENTRY/CF25A.cpp
+++ ENTRY/CF25A.cpp
@@ -1,8 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+int n, a[105], ans, odd, even;
 int main()
 {
-    
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+        if (a[i] % 2 == 0) even++;
+        else odd++;
+    }
+    if (even > odd)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            if (a[i] % 2 == 1)
+            {
+                cout << i;
+                break;
+            }
+        }
+    }
+    else
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            if (a[i] % 2 == 0)
+            {
+                cout << i;
+                break;
+            }
+        }
+    }
     return 0;
 }
