ae768a6e53381e7967b0da27fcd50c8c31ce2cbf
xuxinyang <18729346082@163.com>
2023-03-05 09:53:26 +0800

    fixed P1656

--- PREANDADVANCE/P1656.cpp
+++ PREANDADVANCE/P1656.cpp
@@ -1,30 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int maxn = 155;
-const int maxm = 5005;
+int maps[155][155];
 struct Edge
 {
-    int to, nxt;
-};
-int n, m, u, v, head[maxn], tot;
-Edge edges[maxm];
-
-void add(int u, int v)
+    int x, y;
+} E[5001];
+int dfn[151], low[151], n, m, id, cnt, f[151];
+bool cmp(Edge a, Edge b)
 {
-    edges[++tot].to = v;
-    edges[tot].nxt = head[u];
-    head[u] = tot;
+    if (a.x == b.x)
+        return a.y < b.y;
+    return a.x < b.x;
 }
-
+void addEdge(int x, int y)
+{
+    E[++cnt].x = x;
+    E[cnt].y = y;
+}
+void tarjan(int x)
+{
+    int c = 0, y;
+    dfn[x] = low[x] = ++id;
+    for (register int i = 1; i <= n; i++)
+    {
+        if (!maps[x][i])
+            continue;
+        y = i;
+        if (dfn[y] && y != f[x])
+            low[x] = min(low[x], dfn[y]);
+        if (!dfn[y])
+        {
+            f[y] = x;
+            tarjan(y);
+            low[x] = min(low[x], low[y]);
+            if (low[y] > dfn[x])
+                addEdge(x, y);
+        }
+    }
+}
 int main()
 {
+    int x, y;
     cin >> n >> m;
-    for (int i = 1; i <= m; i++)
+    for (register int i = 1; i <= m; i++)
     {
-        cin >> u >> v;
-        add(u, v);
-        add(v, u);
+        cin >> x >> y;
+        maps[x][y] = maps[y][x] = 1;
     }
-    
+    for (register int i = 1; i <= n; i++)
+    {
+        if (!dfn[i])
+            tarjan(i);
+    }
+    sort(E + 1, E + cnt + 1, cmp);
+    for (register int i = 1; i <= cnt; i++)
+    {
+        cout << min(E[i].x, E[i].y) << ' ' << max(E[i].x, E[i].y) << endl; // 输出
+    }
     return 0;
 }
