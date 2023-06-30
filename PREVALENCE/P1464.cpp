2c90b711407286a68d32f274c664959511e40d03
xuxinyang <18729346082@163.com>
2023-06-30 15:22:22 +0800

    update P1464

--- PREVALENCE/P1464.cpp
+++ PREVALENCE/P1464.cpp
@@ -1,22 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-ll mem[25][25][25];
-ll w(ll, ll, ll);
-ll W(ll x, ll y, ll z)
-{
-    if (mem[x][y][z]) return mem[x][y][z];
-    else return mem[x][y][z] = w(x, y, z);
-}
+ll f[25][25][25];
 ll w(ll a, ll b, ll c)
 {
     if (a <= 0 || b <= 0 || c <= 0)
         return 1;
     if (a > 20 || b > 20 || c > 20)
-        return W(20, 20, 20);
+        return w(20, 20, 20);
+    if (f[a][b][c]) return f[a][b][c];
     if (a < b && b < c)
-        return W(a, b, c - 1) + W(a, b - 1, c - 1) - W(a, b - 1, c);
-    return W(a - 1, b, c) + W(a - 1, b - 1, c) + W(a - 1, b, c - 1) - W(a - 1, b - 1, c - 1);
+        return f[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+    return f[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
 }
 int main()
 {
@@ -25,9 +20,9 @@ int main()
     ll a, b, c;
     while (cin >> a >> b >> c)
     {
-        if (a == -1 && b == -1 && c == -1)
-            return 0;
-        cout << "w(" << a << ", " << b << ", " << c << ") = " << w(a, b, c) << "\n";
+        if (a == -1 && b == -1 && c == -1) return 0;
+        else 
+            cout << "w(" << a << ", " << b << ", " << c << ") = " << w(a,b,c) << "\n" ;
     }
     return 0;
 }
