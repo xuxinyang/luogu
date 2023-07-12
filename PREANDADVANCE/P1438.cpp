8ecaef77012f10288f0b610701307ac8757b90ab
xuxinyang <18729346082@163.com>
2023-07-12 15:21:07 +0800

    fixed P1438

--- PREANDADVANCE/P1438.cpp
+++ PREANDADVANCE/P1438.cpp
@@ -1,86 +1,119 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int maxn = 1e5+5;
-int n, m, a[maxn], opt;
-struct Node
+typedef long long ll;
+int n, m;
+struct SGT
 {
-    int l, r, sum, add;
-};
-Node tree[maxn<<2];
-
-void build(int k, int l, int r)
-{
-    tree[k].l = l, tree[k].r = r;
-    if (l == r)
+    int initval[maxn];
+    ll sumv[maxn<<2];
+    ll addv[maxn<<2];
+    int left, right;
+    ll val;
+    void build() { buildtree(1, 1, n); }
+    void buildtree(int o, int L, int R)
     {
-        tree[k].sum = tree[k].add = a[l];
-        return;
-    }
-    int mid = (l + r) / 2;
-    build(k<<1, l, mid);
-    build(k<<1|1, mid+1, r);
-    tree[k].add = 0;
-    tree[k].sum = tree[k<<1].sum + tree[k<<1|1].sum;
-}
-void update(int k, int l, int r, int v)
-{
-    if (tree[k].l >= l && tree[k].r <= r)
-    {
-        tree[k].add += v;
-        tree[k].sum = tree[k].add * (r - l + 1);
-        if (l != r)
+        if (L == R)
         {
-            tree[k].sum += tree[k<<1].sum + tree[k<<1|1].sum;
-        } 
-        return;
+            sumv[o] = addv[o] = initval[L];
+            return;
+        }
+        int M = L + (R - L) / 2;
+        int lc = o << 1;
+        int rc = lc | 1;
+        buildtree(lc, L, M);
+        buildtree(rc, M + 1, R);
+        addv[o] = 0;
+        sumv[o] = sumv[lc] + sumv[rc];
     }
-    int mid = (tree[k].l + tree[k].r) / 2;
-    if (l <= mid) update(k<<1, l, r, v);
-    if (r > mid) update(k<<1|1, l, r, v);
-    tree[k].sum = tree[k].add * (r - l + 1);
-    if (l != r)
+    void add(int L, int R, ll v)
     {
-        tree[k].sum += tree[k<<1].sum + tree[k<<1|1].sum;
-    } 
-}
-int query(int k, int l, int r, int v)
-{
-    int res = 0;
-    if (tree[k].l >= l && tree[k].r <= r)
-    {
-        res += tree[k].sum + v * (r-l+1);
-        return res;
+        left = L;
+        right = R;
+        val = v;
+        addtree(1, 1, n);
     }
-    int mid = (tree[k].l + tree[k].r) / 2;
-    if (l <= mid) 
-        res += query(k<<1, l, r, v + tree[k].add);
-    if (r > mid)
-        res += query(k<<1|1, l, r, v + tree[k].add);
-    return res;
-}   
+    void addtree(int o, int L, int R)
+    {
+        if (L >= left && R <= right)
+        {
+            addv[o] += val;
+            maintain(o, L, R);
+            return;
+        }
+        int M = L + (R - L) / 2;
+        int lc = o << 1;
+        int rc = lc | 1;
+        if (left <= M)
+            addtree(lc, L, M);
+        if (right > M)
+            addtree(rc, M + 1, R);
+        maintain(o, L, R);
+    }
+    void maintain(int o, int L, int R)
+    {
+        sumv[o] = addv[o] * (R - L + 1);
+        if (L != R)
+        {
+            int lc = o << 1;
+            int rc = lc | 1;
+            sumv[o] += sumv[lc] + sumv[rc];
+        }
+    }
+    ll query(int L, int R)
+    {
+        left = L;
+        right = R;
+        val = 0;
+        querytree(1, 1, n, 0);
+        return val;
+    }
+    void querytree(int o, int L, int R, ll addval)
+    {
+        if (left <= L && right >= R)
+        {
+            val += sumv[o] + addval * (R - L + 1);
+            return;
+        }
+        int M = L + (R - L) / 2;
+        int lc = o << 1;
+        int rc = lc | 1;
+        if (left <= M)
+            querytree(lc, L, M, addval + addv[o]);
+        if (right > M)
+            querytree(rc, M + 1, R, addval + addv[o]);
+    }
+};
+int a[100010];
+SGT sgt;
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int l, r, k, d, p;
-    cin >> n >> m;
-    for (int i = 1; i <= n; i++) cin >> a[i];
-    for (int i = n-1; i >= 1; i--) a[i+1] = a[i+1]-a[i];
-    build(1, 1, n);
-    for (int i = 1; i <= m; i++)
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= n; ++i)
     {
-        cin >> opt;
-        if (opt == 1)
+        scanf("%d", &a[i]);
+        sgt.initval[i] = a[i] - a[i - 1];
+    }
+    sgt.build();
+    for (int i = 0; i < m; ++i)
+    {
+        int order;
+        scanf("%d", &order);
+        if (order == 1)
         {
-            cin >> l >> r >> k >> d;
-            update(1, l, l, k);
-            if (l != r) update(1, l+1, r, d);
-            if (r != n) update(1, r+1, r+1, -k-d*(r-l));
+            int l, r, k, d;
+            scanf("%d%d%d%d", &l, &r, &k, &d);
+            sgt.add(l, l, k);
+            if (l != r)
+                sgt.add(l + 1, r, d);
+            if (r != n)
+                sgt.add(r + 1, r + 1, -k - d * (r - l));
         }
         else
         {
-            cin >> p;
-            cout << query(1, 1, p, 0) << "\n";
+            int p;
+            scanf("%d", &p);
+            cout << sgt.query(1, p) << endl;
         }
     }
     return 0;
