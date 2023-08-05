bf8ae62cd35f6e0fcad9276ee28e239dcc3be26b
xuxinyang <18729346082@163.com>
2023-08-05 16:17:33 +0800

    fixed P4147

--- PREANDADVANCE/P4147.cpp
+++ PREANDADVANCE/P4147.cpp
@@ -3,7 +3,7 @@ using namespace std;
 const int maxn = 1e3+5;
 int l[maxn], r[maxn], a[maxn];
 int n, m, ans;
-char c;
+char c[3];
 int main()
 {
     cin >> n >> m;
@@ -11,23 +11,27 @@ int main()
     {
         for (int j = 1; j <= m; j++)
         {
+            l[j] = r[j] = j;
+        }
+        for (int j = 1; j <= m; j++)
+        {
             cin >> c;
-            if (c == 'R') a[j] = 0;
+            if (c[0] == 'R') a[j] = 0;
             else a[j]++;
         }
+        // 找最大能到达右边界F的个数
+        for (int j = 1; j <= m; j++)
+        {
+            while (l[j] != 1 && a[l[j]-1] >= a[j]) l[j] = l[l[j]-1];
+        }
+        // 找最大能到达左边界F的个数
+        for (int j = m; j >= 1; j--)
+        {
+            while (r[j] != m && a[r[j]+1] >= a[j]) r[j] = r[r[j]+1];
+        }
+        for (int j = 1; j <= m; j++)
+            ans = max(ans,(r[j]-l[j]+1) * a[j]);
     } 
-    // 找最大能到达右边界F的个数
-    for (int j = 1; j <= m; j++)
-    {
-        while (a[l[j]-1] >= a[j]) l[j] = l[l[j]-1];
-    }
-    // 找最大能到达左边界F的个数
-    for (int j = m; j >= 1; j--)
-    {
-        while (a[r[j]+1] >= a[j]) r[j] = r[r[j]+1];
-    }
-    for (int j = 1; j <= m; j++)
-        ans = max(ans, r[j]+l[j]-1) * a[j];
-    cout << ans;
+    cout << ans * 3;
     return 0;
 }
