691e4be248ee62387034350e419353f22a6d06c5
xuxinyang <18729346082@163.com>
2023-08-05 15:35:06 +0800

    fixed P4147

---
+++ PREANDADVANCE/P4147.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn = 1e3+5;
+int l[maxn], r[maxn], a[maxn];
+int n, m, ans;
+char c;
+int main()
+{
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            cin >> c;
+            if (c == 'R') a[j] = 0;
+            else a[j]++;
+        }
+    } 
+    // 找最大能到达右边界F的个数
+    for (int j = 1; j <= m; j++)
+    {
+        while (a[l[j]-1] >= a[j]) l[j] = l[l[j]-1];
+    }
+    // 找最大能到达左边界F的个数
+    for (int j = m; j >= 1; j--)
+    {
+        while (a[r[j]+1] >= a[j]) r[j] = r[r[j]+1];
+    }
+    for (int j = 1; j <= m; j++)
+        ans = max(ans, r[j]+l[j]-1) * a[j];
+    cout << ans;
+    return 0;
+}
