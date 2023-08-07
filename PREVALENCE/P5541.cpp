d0f450a102299b9711efaa02d69cd5c022ed14e1
xuxinyang <18729346082@163.com>
2023-08-07 08:58:36 +0800

    update P5541

--- PREVALENCE/P5541.cpp
+++ PREVALENCE/P5541.cpp
@@ -3,7 +3,13 @@ using namespace std;
 const int maxn = 1e5+5;
 int n, a[maxn], x = 0;
 
-int ansmin()
+/**
+ * @brief 
+ * 找最小值，如果说第1头牛到第n-1头牛的位置中间里够容纳n头牛，且第n-1头牛到第n头牛之间
+ * 能够容纳至少两头牛，那么需要将第1头牛放到第n-1头到n头牛中间，第n头牛放到
+ * @return int 
+ */
+int getmin()
 {
     if ((a[n - 1] - a[1] == n - 2 && a[n] - a[n - 1] > 2) || 
     (a[n] - a[2] == n - 2 && a[2] - a[1] > 2))
@@ -18,7 +24,7 @@ int ansmin()
     return n - x;
 }
 
-int ansmax()
+int getmax()
 {
     return max(a[n - 1] - a[1], a[n] - a[2]) - n + 2;
 }
@@ -28,6 +34,6 @@ int main()
     cin >> n;
     for (int i = 1; i <= n; i++) cin >> a[i];
     sort(a + 1, a + n + 1);
-    cout << ansmin() << "\n" << ansmax() << "\n";
+    cout << getmin() << "\n" << getmax();
     return 0;
 }
