45f575cbb99c6d4f92e3599d8fbd8fa310d99f50
xuxinyang <18729346082@163.com>
2023-08-17 13:48:54 +0800

    fixed P8783

--- PREANDADVANCE/P8783.cpp
+++ PREANDADVANCE/P8783.cpp
@@ -1,16 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-int n, m, k, a[505][505], sum[505][505];
-ll ans;
-ll calc_rec(ll n, ll m)
-{
-    return n * (n - 1) / 2 * m * (m - 1) / 2;
-}
+int n, m, k, a[505][505], sum[505][505], l, r, s[505];
+ll ans, cur;
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
     cin >> n >> m >> k;
     ans = 0;
     for (int i = 1; i <= n; i++)
@@ -18,25 +12,32 @@ int main()
         for (int j = 1; j <= m; j++)
         {
             cin >> a[i][j];
-            sum[i][j] = sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1] + a[i][j];
-            cout << sum[i][j] << " ";
         }
-        cout << "\n";
+    }
+    for (int j = 1; j <= m; j++)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            sum[i][j] = sum[i-1][j] + a[i][j];
+        }
     }
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= m; j++)
+        for (int j = i; j <= n; j++)
         {
-            for (int p = i; p <= n; p++)
+            for (int k = 1; k <= m; k++)
+                s[k] = sum[j][k] - sum[i-1][k]; // i-j行的单列数据和
+            l = 1, r = 1, cur = 0;
+            for (r = 1; r <= m; r++)
             {
-                for (int q = m; q >= j; q--)
-                {
-                    if (sum[p][q]-sum[p][j-1]-sum[i-1][q]+sum[i-1][j-1] <= k)
-                    {
-                        ans += calc_rec(p-i+1, q-j+1);
-                        break;
-                    }
+                cur += s[r];
+                if (cur <= k) ans += r - l + 1;
+                else
+                { 
+                    while (cur > k) cur -= s[l++];
+                    ans += r - l + 1;
                 }
+                
             }
         }
     }
