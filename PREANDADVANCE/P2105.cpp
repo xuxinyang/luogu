07c69f7cb7fe1a2e2e128b0924d2e1f5d2716164
xuxinyang <18729346082@163.com>
2023-08-23 15:03:09 +0800

    P2015 100pts

--- PREANDADVANCE/P2105.cpp
+++ PREANDADVANCE/P2105.cpp
@@ -1,132 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int maxn = 2e4+5;
-bool vis[maxn][maxn];
-int n, m, k, x, y, ans, p;
-struct Pos
-{
-    int x, y;
-};
-Pos pos[505];
-bool check(int x, int y)
-{
-    for (int i = 1; i <= p; i++)
-    {
-        if (pos[i].x == x && pos[i].y == y)
-            return true;
-    }
-    return false;
-}
+int x[505], y[505], n, m, k;
+int flag[maxn], vis[maxn], sum, ans;
 int main()
 {
     cin >> n >> m >> k;
-    ans = n * m;
-    int x1, y1;
     for (int i = 1; i <= k; i++)
+        cin >> x[i] >> y[i], vis[x[i]] = 1;
+    for (int i = 1; i <= n; i++)
     {
-        cin >> x >> y;
-        pos[++p] = Pos{x, y};
-        if (!vis[x][y])
+        if (vis[i]) continue;
+        sum = m;
+        // 判断每一行有多少格子被攻击了
+        for (int j = 1; j <= k; j++)
         {
-            ans--;
-            vis[x][y] = 1;
-        }
-        // up
-        x1 = x-1, y1 = y;
-        while (x1 >= 1)
-        {
-            if (!vis[x1][y1])
+            if (flag[y[j]] != i)
+                sum--;
+            flag[y[j]] = i;
+            if (x[j] < i)
             {
-                ans--;
-                vis[x1][y1] = 1;
+                if (y[j] + i - x[j] >= 1 && y[j] + i - x[j] <= m)
+                {
+                    if (flag[y[j] + i - x[j]] != i)
+                        sum--;
+                    flag[y[j] + i - x[j]] = i;
+                }
+                if (y[j] - i + x[j] >= 1 && y[j] - i + x[j] <= m)
+                {
+                    if (flag[y[j] - i + x[j]] != i)
+                        sum--;
+                    flag[y[j] - i + x[j]] = i;
+                }
             }
-            x1--;
-        }
-        // down
-        x1 = x+1, y1 = y;
-        while (x1 <= n)
-        {
-            if (check(x1, y1)) break;
-            if (!vis[x1][y1])
+            else
             {
-                ans--;
-                vis[x1][y1] = 1;
+                if (y[j] + (x[j] - i) >= 1 && y[j] + (x[j] - i) <= m)
+                {
+                    if (flag[y[j] + (x[j] - i)] != i)
+                        sum--;
+                    flag[y[j] + (x[j] - i)] = i;
+                }
+                if (y[j] - (x[j] - i) >= 1 && y[j] - (x[j] - i) <= m)
+                {
+                    if (flag[y[j] - (x[j] - i)] != i)
+                        sum--;
+                    flag[y[j] - (x[j] - i)] = i;
+                }
             }
-            x1++;
         }
-        // left
-        x1 = x, y1 = y-1;
-        while (y1 >= 1)
-        {
-            if (check(x1, y1)) break;
-            if (!vis[x1][y1])
-            {
-                ans--;
-                vis[x1][y1] = 1;
-            }
-            y1--;
-        }
-        // right
-        x1 = x, y1 = y+1;
-        while (y1 <= m)
-        {
-            if (check(x1, y1)) break;
-            if (!vis[x1][y1])
-            {
-                ans--;
-                vis[x1][y1] = 1;
-            }
-            y1++;
-        }
-        // lu
-        x1 = x-1, y1 = y-1;
-        while (y1 >= 1 && x1 >= 1)
-        {
-            if (check(x1, y1)) break;
-            if (!vis[x1][y1])
-            {
-                ans--;
-                vis[x1][y1] = 1;
-            }
-            y1--, x1--;
-        }
-        // ru
-        x1 = x-1, y1 = y+1;
-        while (y1 <= m && x1 >= 1)
-        {
-            if (check(x1, y1)) break;
-            if (!vis[x1][y1])
-            {
-                ans--;
-                vis[x1][y1] = 1;
-            }
-            y1++, x1--;
-        }
-        // ld
-        x1 = x+1, y1 = y-1;
-        while (y1 >= 1 && x1 <= n)
-        {
-            if (check(x1, y1)) break;
-            if (!vis[x1][y1])
-            {
-                ans--;
-                vis[x1][y1] = 1;
-            }
-            y1--, x1++;
-        }
-        // rd
-        x1 = x+1, y1 = y+1;
-        while (y1 <= m && x1 <= n)
-        {
-            if (check(x1, y1)) break;
-            if (!vis[x1][y1])
-            {
-                ans--;
-                vis[x1][y1] = 1;
-            }
-            y1++, x1++;
-        }
+        ans += sum;
     }
     cout << ans;
-    return 0;
 }
