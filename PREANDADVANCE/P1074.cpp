7b929ab1a7f6a783885472a5dbe9fb0504748416
xuxinyang <18729346082@163.com>
2023-09-01 12:49:40 +0800

    P1074 80pts

--- PREANDADVANCE/P1074.cpp
+++ PREANDADVANCE/P1074.cpp
@@ -11,10 +11,42 @@ const int w[9][9] = {
     {6, 7, 8, 9, 9, 9, 8, 7, 6},
     {6, 7, 8, 8, 8, 8, 8, 7, 6},
     {6, 7, 7, 7, 7, 7, 7, 7, 6},
-    {6, 6, 6, 6, 6, 6, 6, 6, 6}
-};
-int a[9][9], ans;
-
+    {6, 6, 6, 6, 6, 6, 6, 6, 6}};
+int a[9][9], ans = -1, x;
+bool row[10][10], col[10][10], g[10][10];
+void dfs(int idx)
+{
+    if (idx > 80)
+    {
+        int res = 0;
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                res += a[i][j] * w[i][j];
+            }
+        }
+        ans = max(ans, res);
+        return;
+    }
+    int fx = idx / 9, fy = idx % 9;
+    if (a[fx][fy])
+        dfs(idx + 1);
+    else
+    {
+        for (int val = 1; val <= 9; val++)
+        {
+            if (!row[fx][val] && !col[fy][val] && !g[fx/3*3+fy/3][val])
+            {
+                row[fx][val] = col[fy][val] = g[fx/3*3+fy/3][val] = 1;
+                a[fx][fy] = val;
+                dfs(idx+1);
+                a[fx][fy] = 0;
+                row[fx][val] = col[fy][val] = g[fx/3*3+fy/3][val] = 0;
+            }
+        }
+    }
+}
 int main()
 {
     for (int i = 0; i < 9; i++)
@@ -22,8 +54,11 @@ int main()
         for (int j = 0; j < 9; j++)
         {
             cin >> a[i][j];
+            x = a[i][j];
+            row[i][x] = col[j][x] = g[i / 3 * 3 + j / 3][x] = 1;
         }
     }
-    
+    dfs(0);
+    cout << ans;
     return 0;
 }
