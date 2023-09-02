60c1c6dd5c7190eef508880823fa79ac6e594848
xuxinyang <18729346082@163.com>
2023-09-02 13:06:20 +0800

    fixed P1074

--- PREANDADVANCE/P1074.cpp
+++ PREANDADVANCE/P1074.cpp
@@ -1,7 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int dx[4] = {0, 1, 0, -1};
-const int dy[4] = {-1, 0, 1, 0};
 const int w[9][9] = {
     {6, 6, 6, 6, 6, 6, 6, 6, 6},
     {6, 7, 7, 7, 7, 7, 7, 7, 6},
@@ -12,7 +10,12 @@ const int w[9][9] = {
     {6, 7, 8, 8, 8, 8, 8, 7, 6},
     {6, 7, 7, 7, 7, 7, 7, 7, 6},
     {6, 6, 6, 6, 6, 6, 6, 6, 6}};
-int a[9][9], ans = -1, x;
+int a[9][9], ans = -1, x, p, cnt[10][10], c;
+struct HL
+{
+    int val, k;
+};
+HL hl[100];
 bool row[10][10], col[10][10], g[10][10];
 void dfs(int idx)
 {
@@ -29,7 +32,7 @@ void dfs(int idx)
         ans = max(ans, res);
         return;
     }
-    int fx = idx / 9, fy = idx % 9;
+    int fx = hl[idx].k / 9, fy = hl[idx].k % 9;
     if (a[fx][fy])
         dfs(idx + 1);
     else
@@ -56,8 +59,46 @@ int main()
             cin >> a[i][j];
             x = a[i][j];
             row[i][x] = col[j][x] = g[i / 3 * 3 + j / 3][x] = 1;
+
         }
     }
+    // 统计每一行0的个数
+    for (int i = 0; i < 9; i++)
+    {
+        int t = 0;
+        for (int j = 0; j < 9; j++)
+        {
+            if (a[i][j]) t++;
+        }
+        cnt[i][9] = t;
+    }
+    // 统计每一列0的个数
+    for (int i = 0; i < 9; i++)
+    {
+        int t = 0;
+        for (int j = 0; j < 9; j++)
+        {
+            if (a[j][i]) t++;
+        }
+        cnt[9][i] = t;
+    }
+    // 每一行每一列0的个数
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            cnt[i][j] = cnt[i][9] + cnt[9][j];
+            hl[p++] = HL{cnt[i][j], i * 9 + j};
+        }
+    }
+    sort(hl, hl + p, [](HL x, HL y){
+        if (x.val == y.val)
+        {
+            if (x.k/9 == y.k/9) return x.k%9 < y.k%9;
+            else return x.k/9 < y.k/9;
+        }
+        return x.val > y.val;
+    });
     dfs(0);
     cout << ans;
     return 0;
