83b7eaf2c4902b5b2e907a76d26f31ab8fcf4c3b
xuxinyang <18729346082@163.com>
2023-10-26 14:22:04 +0800

    fixed P4375

--- ADVANCANDPROVINCE/P4375.cpp
+++ ADVANCANDPROVINCE/P4375.cpp
@@ -2,31 +2,45 @@
 using namespace std;
 const int maxn = 1e5+5;
 int n, a[maxn], ans;
+struct Num
+{
+    int val, id;
+};
+Num nums[maxn];
+void update(int x)
+{
+    while (x <= n)
+    {
+        a[x]++;
+        x += x&-x;
+    }
+}
+int query(int x)
+{
+    int res = 0;
+    while (x)
+    {
+        res += a[x];
+        x -= x&-x;
+    }
+    return res;
+}
 int main()
 {
     cin >> n;
-    for (int i = 1; i <= n; i++) cin >> a[i];
-    for (int i = 1; i <= n - 1; i++)
+    for (int i = 1; i <= n; i++)
     {
-        bool flag = true;
-        for (int j = 1; j <= n-i; j++)
-        {
-            if (a[j] > a[j+1]) 
-            {
-                swap(a[j], a[j+1]);
-                flag = 0;
-            }
-        }
-        for (int j = n-i; j >= 1; j--)
-        {
-            if (a[j] > a[j+1]) 
-            {
-                swap(a[j], a[j+1]);
-                flag = 0;
-            }
-        }
-        if (flag) break;
-        else ans++;
+        cin >> nums[i].val;
+        nums[i].id = i;
+    }
+    sort(nums + 1, nums + n + 1, [](Num x, Num y){
+        return x.val == y.val ? x.id < y.id : x.val < y.val;
+    });
+    ans = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        update(nums[i].id);
+        ans = max(ans, i-query(i));
     }
     cout << ans;
     return 0;
