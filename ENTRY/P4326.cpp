1904fe6035b826f33bbfe944ef28e5cd56bb2111
xuxinyang <18729346082@163.com>
2023-11-15 18:28:07 +0800

    fixed P4326

---
+++ ENTRY/P4326.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pi 3.141592653589793
+int main()
+{
+    double r;
+    cin >> r;
+    cout << fixed << setprecision(6) << pi * r * r << endl;
+    cout << fixed << setprecision(6) << 2 * r * r << endl;
+    return 0;
+}
