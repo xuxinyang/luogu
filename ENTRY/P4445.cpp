28d7819e79e9e9994c791f8e8b72ad266d0f752d
xuxinyang <18729346082@163.com>
2023-11-15 19:03:53 +0800

    update P4445

--- ENTRY/P4445.cpp
+++ ENTRY/P4445.cpp
@@ -8,7 +8,7 @@ int main()
 {
     cin >> n;
     for (int i = 1; i <= n; i++) cin >> a[i];
-    for (int i = 1; i < n; i++) ans += max(a[i], a[i-1]);
+    for (int i = 2; i <= n; i++) ans += max(a[i], a[i-1]);
     cout << ans;
     return 0;
 }
