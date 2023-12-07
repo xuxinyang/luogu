32785c9dfd95c6f6d8aaeb58d8f4955989b7d0b3
caomuyilin <18729346082@163.com>
2023-12-07 14:55:01 +0800

    fixed P7441

---
+++ PREVALENCE/P7441.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int t, x, y, k;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> t;
+    while (t--)
+    {
+        cin >> x >> y >> k;
+        if (x > y) swap(x, y);
+        if (x == 0 && y != 0)
+        {
+            if (k % y == 0) cout << "1\n";
+            else cout << "0\n";
+            continue;
+        }
+        if (x == 0 && y == 0)
+        {
+            cout << "0\n";
+            continue;
+        }
+        int tmp = (k - (k / x) * x) / y;
+        cout << (k / y) - tmp << "\n";
+    }
+}
