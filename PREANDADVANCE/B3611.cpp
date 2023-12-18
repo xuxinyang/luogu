a0f86ba3170550a991549b470b387280c74095ee
caomuyilin <18729346082@163.com>
2023-12-18 18:17:01 +0800

    fixed B3611

---
+++ PREANDADVANCE/B3611.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, a[105][105];
+int main()
+{
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    for (int k = 1; k <= n; k++)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                a[i][j] = a[i][j] | a[i][k] & a[k][j];
+            }
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
