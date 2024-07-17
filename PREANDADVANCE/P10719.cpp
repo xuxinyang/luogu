4336ad5f38f5388cc1ead1d8e6589b2a90c6e1c5
caomuyilin <18729346082@163.com>
2024-07-17 11:11:52 +0800

    fixed P10719

--- PREANDADVANCE/P10719.cpp
+++ PREANDADVANCE/P10719.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, ans;
+int n, m, k, ans;
 char c;
-int sum[15][15];
+int sum[105][105];
 int main()
 {
-    cin >> n >> m;
+    cin >> n >> m >> k;
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= m; j++)
@@ -14,6 +14,7 @@ int main()
             sum[i][j] = sum[i-1][j]+sum[i][j-1]-sum[i-1][j-1]+(c - '0');
         }
     }
+    ans = INT_MAX;
     for (int r = 1; r <= n; r++)    // 枚举矩形的高
     {
         for (int c = 1; c <= m; c++)  // 枚举矩形的宽
@@ -24,13 +25,15 @@ int main()
                 for (int j = 1; j <= m-c+1; j++)
                 {
                     // 验证是否满足要求，即
-                    //r*c == 2*(sum[i+r-1][j+c-1]-sum[i+r-1][j-1]-sum[i-1][j+c-1]+sum[i-1][j-1])
-                    if (r*c == 2*(sum[i+r-1][j+c-1]-sum[i+r-1][j-1]-sum[i-1][j+c-1]+sum[i-1][j-1]))
-                        ans = max(ans, r*c);
+                    if (k <= (sum[i+r-1][j+c-1]-sum[i+r-1][j-1]-sum[i-1][j+c-1]+sum[i-1][j-1]))
+                    {
+                        ans = min(r*c, ans);
+                    }
                 }
             }
         }
     }
-    cout << ans;
+    if (ans == INT_MAX) cout << 0;
+    else cout << ans;
     return 0;
 }
