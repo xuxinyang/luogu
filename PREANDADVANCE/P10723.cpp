a75105bf0f16707576e4075d6157e942c4d98742
caomuyilin <18729346082@163.com>
2024-10-13 16:09:47 +0800

    fixed P10723

--- PREANDADVANCE/P10723.cpp
+++ PREANDADVANCE/P10723.cpp
@@ -1,33 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int maxn = 1e5+5;
-int n, a[maxn], cntz, cntc, start, vis[maxn];
+int n, root, cnt1, vis[maxn], ans;
 vector<int> ve[maxn];
 void add(int u, int v)
 {
     ve[u].push_back(v);
 }
-void dfs(int u)
+void dfs(int u, int f)
 {
-    vis[u] = 1;
-    for (int i = 0; i < ve[u].size(); i++)
+    for (int v : ve[u])
     {
-        int v = ve[u][i];
-        if (!vis[v])
-        {
-            if (ve[v].size() == 1 && a[v] == 0) cntc++;
-            dfs(v);
-        }
+        if (v == f) continue;
+        dfs(v, u);
+        vis[u] += vis[v];
     }
 }
 int main()
 {
     cin >> n;
+    int x;
     for (int i = 1; i <= n; i++)
     {
-        cin >> a[i];
-        if (a[i] == 0) cntz++;
-        if (a[i] == 1) start = i;
+        cin >> x;
+        if (x == 1) vis[i] = 1, cnt1++, root = i;
     }
     int u, v;
     for (int i = 1; i <= n-1; i++)
@@ -36,7 +32,8 @@ int main()
         add(u, v);
         add(v, u);
     }
-    dfs(start);
-    cout << cntz - cntc;
+    dfs(root, 0);
+    for (int i = 1; i <= n; i++) ans += bool(vis[i]);
+    cout << ans - cnt1;
     return 0;
 }
