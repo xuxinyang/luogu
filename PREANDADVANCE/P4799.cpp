f6463d96f2cee0de73dd0f675595483563d67158
caomuyilin <18729346082@163.com>
2024-11-28 12:37:47 +0800

    fixed P4799

---
+++ PREANDADVANCE/P4799.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int maxn = 45;
+ll n, m, a[maxn], f[maxn];
+void dfs(int idx, int cur, ll sum)
+{
+
+}
+int main()
+{
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    sort(a+1, a+n+1);
+    for (int i = 1; i <= n; i++)
+    {
+
+    }
+    return 0;
+}
