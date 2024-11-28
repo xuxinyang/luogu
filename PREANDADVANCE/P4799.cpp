7579ad24bcbdfd02fbdb961b6c50e25de664549f
caomuyilin <18729346082@163.com>
2024-11-28 15:35:29 +0800

    fixed P4799

--- PREANDADVANCE/P4799.cpp
+++ PREANDADVANCE/P4799.cpp
@@ -1,20 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-const int maxn = 45;
-ll n, m, a[maxn], f[maxn];
-void dfs(int idx, int cur, ll sum)
+const int maxn = 55;
+const int maxv = 2e6+5;
+ll n, m, a[maxn], f[maxv], g[maxv], ans;
+int p, q;
+void dfs(int l, int r, ll d[], ll sum, int &idx)
 {
-
+    if (sum > m) return;
+    if (l > r) {d[++idx] = sum; return;}
+    dfs(l+1, r, d, sum+a[l], idx);
+    dfs(l+1, r, d, sum, idx);
 }
 int main()
 {
     cin >> n >> m;
     for (int i = 1; i <= n; i++) cin >> a[i];
-    sort(a+1, a+n+1);
-    for (int i = 1; i <= n; i++)
+    int mid = (1 + n) >> 1;
+    dfs(1, mid, f, 0, p);
+    dfs(mid+1, n, g, 0, q);
+    sort(g+1, g+q+1);
+    for (int i = 1; i <= p; i++)
     {
-
+        ans += upper_bound(g+1, g+q+1, m-f[i])-g-1;
     }
+    cout << ans;
     return 0;
 }
