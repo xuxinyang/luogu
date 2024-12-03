e1d58ec25855986c1348d633dee77b1e07c3b31a
caomuyilin <18729346082@163.com>
2024-12-03 20:17:58 +0800

    update P9752

--- PREVALENCE/P9752.cpp
+++ PREVALENCE/P9752.cpp
@@ -4,6 +4,8 @@ const int maxn = 10;
 int n, f[maxn][maxn][maxn][maxn][maxn], a, b, c, d, e, ans;
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
     cin >> n;
     for (int i = 1; i <= n; i++)
     {
@@ -15,17 +17,21 @@ int main()
         for (int j = 0; j <= 9; j++) f[a][b][c][j][e]++;
         for (int j = 0; j <= 9; j++) f[a][b][c][d][j]++;
         // 转动两个拨轮
-        for (int j = 1; j <= 9; j++) f[(a+j)%10][(b+j)%10][c][d][e]++;
-        for (int j = 1; j <= 9; j++) f[a][(b+j)%10][(c+j)%10][d][e]++;
-        for (int j = 1; j <= 9; j++) f[a][b][(c+j)%10][(d+j)%10][e]++;
-        for (int j = 1; j <= 9; j++) f[a][b][c][(d+j)%10][(e+j)%10]++;
+        for (int j = 0; j <= 9; j++) f[(a+j)%10][(b+j)%10][c][d][e]++;
+        for (int j = 0; j <= 9; j++) f[a][(b+j)%10][(c+j)%10][d][e]++;
+        for (int j = 0; j <= 9; j++) f[a][b][(c+j)%10][(d+j)%10][e]++;
+        for (int j = 0; j <= 9; j++) f[a][b][c][(d+j)%10][(e+j)%10]++;
     }
     for (int i = 0; i <= 9; i++)
         for (int j = 0; j <= 9; j++)
             for (int k = 0; k <= 9; k++)
                 for (int p = 0; p <= 9; p++)
                     for (int q = 0; q <= 9; q++)
-                        if (f[i][j][k][p][q] == n) ans++;
+                        if (f[i][j][k][p][q] == n)
+                        {
+                            // printf("%d %d %d %d %d\n", i, j, k, p, q);
+                            ans++;
+                        }
     cout << ans;
     return 0;
 }
