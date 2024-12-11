340bebc64b7a2cd14240c6fb30a87fc33fc4213e
caomuyilin <18729346082@163.com>
2024-12-11 17:29:44 +0800

    fixed P7075

--- PREANDADVANCE/P7075.cpp
+++ PREANDADVANCE/P7075.cpp
@@ -19,22 +19,22 @@ int main()
             if (is_leap(y[i]) && d[i] > 29) m[i]++, d[i] = 1;
             else if (!is_leap(y[i]) && d[i] > 28) m[i]++, d[i] = 1;
         }
-        else if (d[i] > mons[m[i]-1]) m[i]++, d[i] = 1;
+        else {if (d[i] > mons[m[i]-1]) m[i]++, d[i] = 1;}
         if (m[i] > 12) y[i]++, m[i] = 1;
     }
     scanf("%lld", &q);
     while (q--)
     {
         scanf("%lld", &r);
-        if (r > 2299160)
+        if (r > 2299160)    // 大于儒略历天数，在格里高历
         {
-            r -= 2159351;
+            r -= 2159351;   // 2159351（跳过的天数）=2159358（1200年1月1日儒略历）+3（补上1300、1400、1500三年闰年）-10（10天删除日期）
             t = r/maxn*400+1200;
             r %= maxn;
         }
         else
         {
-            t = r % 1461*4-4712;
+            t = r / 1461*4-4712;
             r %= 1461;
         }
         if (t + y[r] > 0) printf("%lld %lld %lld\n", d[r], m[r], t+y[r]);
