07058a00139844370720b303953ddbd44f1e4420
caomuyilin <18729346082@163.com>
2024-12-18 14:10:49 +0800

    fixed P10274

--- PREANDADVANCE/P10274.cpp
+++ PREANDADVANCE/P10274.cpp
@@ -9,24 +9,24 @@ void init()
 	s[0] = "or";
 	for (int i = 1; i <= n; i+=2)
 	{
-		if (s[i-1] == "or") 
+		if (s[i-1] == "or")     // 新的一段
 		{
-			b[i] = ++cnt;
-			c[cnt] = i;
-			bp[i] = 1; 
-			a[cnt] = (s[i]=="true");
-			ap[i] = ap[c[b[i]-1]]|a[cnt-1];
+			b[i] = ++cnt;   // b[i] 存储当前处于第几段
+			c[cnt] = i;     // c[cnt] 存储当前这一段的起始位置
+			bp[i] = 1;      // i 位置前面所有块 and 起来的值初始化为1
+			a[cnt] = (s[i]=="true");    // 第cnt块的值
+			ap[i] = ap[c[b[i]-1]]|a[cnt-1]; // i 位置前面所有块 or 起来的值 = 上一块位置前面所有块 or 起来的值 or 上一块的值
 		}
 		else
 		{
-			b[i] = cnt;
-			bp[i] = a[cnt];
-			a[cnt] &= (s[i]=="true");
-			ap[i] = ap[c[b[i]-1]]|a[cnt-1];
+			b[i] = cnt; // 处于哪一块
+			bp[i] = a[cnt]; // i 位置前面所有块的值 and 起来的值 = 第cnt块的值
+			a[cnt] &= (s[i]=="true");   // 更新第cnt块的值
+			ap[i] = ap[c[b[i]-1]]|a[cnt-1]; // i 位置前面所有块 or 起来的值 = 上一块位置前面所有块 or 起来的值 or 上一块的值
 		}
 	}
 	s[n+1] = "or";
-	cnt = 0;
+	cnt = 0;    // 从后往前扫描一遍
 	for (int i = n; i >= 1; i -= 2)
 	{
 		if (s[i+1] == "or")
