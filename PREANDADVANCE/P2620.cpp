f361d0e6fe2704c9c7ed9972ca5557843339a1eb
caomuyilin <18729346082@163.com>
2024-12-18 16:24:39 +0800

    fixed P2620

--- PREANDADVANCE/P2620.cpp
+++ PREANDADVANCE/P2620.cpp
@@ -1,61 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
-int w, s, p, b, e;
-unordered_map<int, int> ump;
-unordered_map<int, bool> vis;
-void init()
+const int inf = 0x3f3f3f3f;
+int W, S, P;
+struct Point
 {
-    ump.clear();
-    vis.clear();
-    for (int i = 1; i <= p; i++)
-    {
-        cin >> b >> e;
-        ump[b] = e;
-    }
-}
-struct Pos
-{
-    int pos, step;
+    int s, t;
 };
-void bfs()
+Point a[45];
+map<int, int> f;
+int ans;
+void dfs(int x, int v)
 {
-    init();
-    queue<Pos> q;
-    q.push(Pos{0, 0});
-    vis[0] = 1;
-    while (!q.empty())
+    if (v >= f[x])
+        return;
+    f[x] = v;
+    if (x > a[P].s || P == 0)
+    { 
+        ans = min(ans, v + (int)ceil(1.0 * (W - x) / S));
+        return;
+    }
+    for (int i = 1; i <= P; i++)
     {
-        Pos pos = q.front();
-        q.pop();
-        if (pos.pos == w)
+        if (a[i].s < x)
+            continue;
+        if (a[i].s == x)
         {
-            cout << pos.step << "\n";
-            break;
+            dfs(a[i].t, v);
+            return;
         }
-        for (int i = 1; i <= s; i++)
-        {
-            if (vis.count(pos.pos + i) == 0)
-            {
-                vis[pos.pos+i] = 1;
-                if (ump.count(pos.pos+i) != 0)
-                {
-                    vis[ump[pos.pos+i]] = 1;
-                    q.push(Pos{ump[pos.pos+i], pos.step+1});
-                }
-                else q.push(Pos{pos.pos+i, pos.step+1});
-            }
+        int t = ceil(1.0 * (a[i].s - x) / S);
+        dfs(a[i].t, v + t); // 刚好落在虫洞时的最少步数，并且传送到另一端。
+        for (int j = a[i].s + 1; j <= a[i].s + S - 1; j++)
+        {                       // 跨越虫洞，枚举落脚点。
+            if (j <= t * S + x) // 分类讨论，推式子。
+                dfs(j, v + (a[i].s - x) / S + 1);
+            else
+                dfs(j, v + t + 1);
         }
+        break;
     }
 }
-int main()
+signed main()
 {
     ios::sync_with_stdio(false);
-    cin.tie(0);
-    while (cin >> w)
+    cin.tie();
+    while (cin >> W && W)
     {
-        if (w == 0) break;
-        cin >> s >> p;
-        bfs();
+        cin >> S >> P;
+        f.clear();
+        for (int i = 1; i <= P; i++)
+        {
+            cin >> a[i].s >> a[i].t;
+            f[a[i].s] = f[a[i].t] = 1e9;
+            for (int j = 1; j <= S - 1; j++) f[a[i].s + j] = 1e9;
+        }
+        sort(a + 1, a + P + 1, [](Point &a, Point &b){
+            return a.s < b.s;
+        });
+        f[0] = inf, ans = inf;
+        dfs(0, 0);
+        cout << ans << "\n";
     }
     return 0;
 }
