614a7e4303e7a6319d80ddf11454e4b8a8697599
caomuyilin <18729346082@163.com>
2024-12-18 17:31:12 +0800

    fixed P4722

--- NOICTSC/P4722.cpp
+++ NOICTSC/P4722.cpp
@@ -1,106 +1,133 @@
-#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <queue>
+#include <stack>
 using namespace std;
-const int maxn = 1e3+205;
-const int maxm = 1e5+2e4+5;
-const int inf = INT_MAX;
+constexpr int N = 1200, M = 120000, INF = 0x3f3f3f3f;
 int n, m, s, t;
-int cnt, head[maxn], pre[maxn], h[maxn], g[maxn];
-struct Edge
+
+struct qxx
 {
-	int v, next, cap, flow;
-}edges[maxm<<1];
-void init()
+    int nex, t;
+    long long v;
+};
+
+qxx e[M * 2 + 1];
+int h[N + 1], cnt = 1;
+
+void add_path(int f, int t, long long v)
 {
-	memset(head, -1, sizeof(head));
-	cnt = 0;
+    e[++cnt] = qxx{h[f], t, v}, h[f] = cnt;
 }
-void add(int u, int v, int c)
+
+void add_flow(int f, int t, long long v)
 {
-	edges[cnt] = Edge{v, head[u], c, 0};
-	head[u] = cnt++;
+    add_path(f, t, v);
+    add_path(t, f, 0);
 }
-void set_h(int t, int n)
+
+int ht[N + 1];       // 高度;
+long long ex[N + 1]; // 超额流;
+int gap[N];          // gap 优化. gap[i] 为高度为 i 的节点的数量
+stack<int> B[N];     // 桶 B[i] 中记录所有 ht[v]==i 的v
+int level = 0;       // 溢出节点的最高高度
+
+int push(int u)
+{                       // 尽可能通过能够推送的边推送超额流
+    bool init = u == s; // 是否在初始化
+    for (int i = h[u]; i; i = e[i].nex)
+    {
+        const int &v = e[i].t;
+        const long long &w = e[i].v;
+        // 初始化时不考虑高度差为1
+        if (!w || (init == false && ht[u] != ht[v] + 1) || ht[v] == INF)
+            continue;
+        long long k = init ? w : min(w, ex[u]);
+        // 取到剩余容量和超额流的最小值，初始化时可以使源的溢出量为负数。
+        if (v != s && v != t && !ex[v])
+            B[ht[v]].push(v), level = max(level, ht[v]);
+        ex[u] -= k, ex[v] += k, e[i].v -= k, e[i ^ 1].v += k; // push
+        if (!ex[u])
+            return 0; // 如果已经推送完就返回
+    }
+    return 1;
+}
+
+void relabel(int u)
+{ // 重贴标签（高度）
+    ht[u] = INF;
+    for (int i = h[u]; i; i = e[i].nex)
+        if (e[i].v)
+            ht[u] = min(ht[u], ht[e[i].t]);
+    if (++ht[u] < n)
+    { // 只处理高度小于 n 的节点
+        B[ht[u]].push(u);
+        level = max(level, ht[u]);
+        ++gap[ht[u]]; // 新的高度，更新 gap
+    }
+}
+
+bool bfs_init()
 {
-	queue<int> q;
-	memset(h, -1, sizeof(h));
-	memset(g, 0, sizeof(g));
-	h[t] = 0;
-	q.push(t);
-	while (!q.empty())
-	{
-		int u = q.front();
-		q.pop();
-		++g[h[u]];
-		for (int i = head[u]; ~i; i = edges[i].next)
-		{
-			int v = edges[i].v;
-			if (h[v] == -1)
-			{
-				h[v] = h[u] + 1;
-				q.push(v);
-			}
-		}
-	}
+    memset(ht, 0x3f, sizeof(ht));
+    queue<int> q;
+    q.push(t), ht[t] = 0;
+    while (q.size())
+    { // 反向 BFS, 遇到没有访问过的结点就入队
+        int u = q.front();
+        q.pop();
+        for (int i = h[u]; i; i = e[i].nex)
+        {
+            const int &v = e[i].t;
+            if (e[i ^ 1].v && ht[v] > ht[u] + 1)
+                ht[v] = ht[u] + 1, q.push(v);
+        }
+    }
+    return ht[s] != INF; // 如果图不连通，返回 0
 }
-int ISAP(int s, int t, int n)
+
+// 选出当前高度最大的节点之一, 如果已经没有溢出节点返回 0
+int select()
 {
-	set_h(t, n);
-	int ans = 0, u = s, d;
-	while (h[s] < n)
-	{
-		int i = head[u];
-		if (u == s) d = inf;
-		for (; ~i; i = edges[i].next)
-		{
-			int v = edges[i].v;
-			if (edges[i].cap > edges[i].flow && h[u] == h[v] + 1)
-			{
-				u = v;
-				pre[v] = i;
-				d = min(d, edges[i].cap - edges[i].flow);
-				if (u == t)
-				{
-					while (u != s)
-					{
-						int j = pre[u];
-						edges[j].flow += d;
-						edges[j^1].flow -= d;
-						u = edges[j^1].v;
-					}
-					ans += d;
-					d = inf;
-					d = inf;
-				}
-				break;
-			}
-		}
-		if (i == -1)
-		{
-			if (--g[h[u]] == 0) break;
-			int hmin = n - 1;
-			for (int j = head[u]; ~j; j = edges[j].next)
-			{
-				if (edges[j].cap > edges[j].flow)
-					hmin = min(hmin, h[edges[j].v]);
-			}
-			h[u] = hmin + 1;
-			++g[h[u]];
-			if (u != s) u = edges[pre[u]^1].v;
-		}
-	}
-	return ans;
+    while (level > -1 && B[level].size() == 0)
+        level--;
+    return level == -1 ? 0 : B[level].top();
 }
+
+long long hlpp()
+{ // 返回最大流
+    if (!bfs_init())
+        return 0; // 图不连通
+    memset(gap, 0, sizeof(gap));
+    for (int i = 1; i <= n; i++)
+        if (ht[i] != INF)
+            gap[ht[i]]++; // 初始化 gap
+    ht[s] = n;
+    push(s); // 初始化预流
+    int u;
+    while ((u = select()))
+    {
+        B[level].pop();
+        if (push(u))
+        { // 仍然溢出
+            if (!--gap[ht[u]])
+                for (int i = 1; i <= n; i++)
+                    if (i != s && ht[i] > ht[u] && ht[i] < n + 1)
+                        ht[i] = n + 1; // 这里重贴成 n+1 的节点都不是溢出节点
+            relabel(u);
+        }
+    }
+    return ex[t];
+}
+
 int main()
 {
-	int u, v, c;
-	cin >> n >> m >> s >> t;
-	init();
-	for (int i = 1; i <= m; i++)
-	{
-		cin >> u >> v >> c;
-		add(u, v, c);
-		add(v, u, 0);
-	}
-	cout << ISAP(s, t, n);
-	return 0;
+    scanf("%d%d%d%d", &n, &m, &s, &t);
+    for (int i = 1, u, v, w; i <= m; i++)
+    {
+        scanf("%d%d%d", &u, &v, &w);
+        add_flow(u, v, w);
+    }
+    printf("%lld", hlpp());
+    return 0;
 }
