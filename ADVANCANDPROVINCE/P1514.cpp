19fc47277a2b119740dc69645e0b72262b9ac26d
caomuyilin <18729346082@163.com>
2025-01-04 11:06:14 +0800

    fixed P1514

--- ADVANCANDPROVINCE/P1514.cpp
+++ ADVANCANDPROVINCE/P1514.cpp
@@ -2,56 +2,50 @@
 using namespace std;
 const int maxn = 505;
 const int dis[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-int n, m, a[maxn][maxn], cnt, res;
+int n, m, a[maxn][maxn], ans;
 bool vis[maxn][maxn];
-priority_queue<pair<int, pair<int, int>> > pq;
+int l[maxn][maxn], r[maxn][maxn];
 void dfs(int x, int y)
 {
     // cout << x << " " << y << "\n";
+    vis[x][y] = 1;
     for (int i = 0; i < 4; i++)
     {
         int nx = x + dis[i][0], ny = y + dis[i][1];
-        if (nx >= 1 && nx <= n && ny >= 1 && ny <= m && !vis[nx][ny]
-        && a[nx][ny] < a[x][y])
-        {
-            vis[nx][ny] = 1;
-            if (nx == n) cnt++;
-            dfs(nx, ny);
-        }
+        if (nx < 1 || nx > n || ny < 1 || ny > m || a[nx][ny] >= a[x][y]) continue; 
+        if (!vis[nx][ny]) dfs(nx, ny);
+        l[x][y] = min(l[x][y], l[nx][ny]);
+        r[x][y] = max(r[x][y], r[nx][ny]);
     }
 }
 int main()
 {
-    freopen("1.in", "r", stdin);
-    freopen("1.out", "w", stdout);
+    // freopen("1.in", "r", stdin);
+    // freopen("1.out", "w", stdout);
     cin >> n >> m;
+    memset(l, 0x3f, sizeof(l));
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= m; j++)
         {
             cin >> a[i][j];
-            if (i == 1) pq.push(make_pair(a[i][j], make_pair(i, j)));
+            if (i == n) l[i][j] = r[i][j] = j;
         }
     }
-    while (cnt < m && !pq.empty())
+    for (int i = 1; i <= m; i++) if (!vis[1][i]) dfs(1, i);
+    int f = 1;
+    for (int i = 1; i <= m; i++) if (!vis[n][i]) ans++, f = 0;
+    if (!f) {cout << "0\n" << ans << "\n"; return 0;}
+    int le = 1, ri = r[1][1];
+    while (le <= m)
     {
-        auto cur = pq.top();
-        int x = cur.second.first, y = cur.second.second;
-        if (cnt == m || pq.empty()) break;
-        while (vis[x][y] && !pq.empty()) pq.pop(), cur = pq.top(), x = cur.second.first, y = cur.second.second;
-        vis[x][y] = 1;
-        res++;
-        dfs(x, y);
+        for (int i = 1; i <= m; i++)
+        {
+            if (l[1][i] <= le) ri = max(ri, r[1][i]);
+        }
+        le = ri+1;
+        ans++;
     }
-    if (cnt == m)
-    {
-        cout << "1\n";
-        cout << res << "\n";
-    }
-    else
-    {
-        cout << "0\n";
-        cout << m-cnt << "\n";
-    }
+    cout << "1\n" << ans << "\n";
     return 0;
 }
