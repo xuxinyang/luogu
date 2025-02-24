7af5c783a24d26f9d7fa689caaec873deaa09930
caomuyilin <18729346082@163.com>
2025-02-24 19:00:27 +0800

    fixed ABC075C

--- PREANDADVANCE/ABC075C.cpp
+++ PREANDADVANCE/ABC075C.cpp
@@ -1,41 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int N = 55;
-const int M = N*N/2;
-struct Edge
+int n, m, cnt, idx, dfn[N], low;
+vector<int> g[N];
+int tarjan(int u, int fa)
 {
-    int u, v;
-};
-Edge edges[M];
-int n, m, cnt, fa[N];
-int find(int x)
-{
-    return x == fa[x] ? x : fa[x] = find(fa[x]);
-}
-void merge(int u, int v)
-{
-    int fu = find(u), fv = find(v);
-    if (fu != fv) fa[fu] = fv;
+    int lowu = dfn[u] = ++idx;
+    for (int v : g[u])
+    {
+        if (!dfn[v])
+        {
+            int lowv = tarjan(v, u);
+            lowu = min(lowu, lowv);
+            if (lowv > dfn[u])
+                cnt++;
+        }
+        else if (v != fa)
+            lowu = min(lowu, dfn[v]);
+    }
+    return lowu;
 }
 int main()
 {
     cin >> n >> m;
-    for (int i = 1; i <= m; i++)
+    for (int i = 0; i < m; i++)
     {
-        cin >> edges[i].u >> edges[i].v;
+        int u, v;
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
     }
-    for (int i = 1; i <= m; i++)
-    {
-        memset(fa, 0, sizeof(fa));
-        for (int j = 1; j <= n; j++) fa[j] = j;
-        for (int j = 1; j <= m; j++)
-        {
-            if (i == j) continue;
-            merge(edges[j].u, edges[j].v);
-        }
-        int u = edges[i].u, v = edges[i].v;
-        if (find(u) != find(v)) cnt++;
-    }
+    for (int i = 1; i <= n; i++)
+        if (!dfn[i])
+            low = tarjan(i, 0);
     cout << cnt << endl;
     return 0;
 }
