510341062982d322803a8757b8151f90e2373d50
caomuyilin <18729346082@163.com>
2025-03-25 15:24:54 +0800

    fixed P11963

---
+++ PREANDADVANCE/P11963.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int maxn = 2e5+5;
+const ll inf = 1e18;
+ll n, a[maxn], b[maxn], dp[maxn], sum, ans, maxx, minx;
+int main()
+{
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+        sum += a[i];
+        b[i] = -a[i];
+    }
+    maxx = -inf, minx = -inf;
+    // 环上最大子段和
+    for (int i = 1; i <= n; i++)
+    {
+        dp[i] = max(dp[i-1]+a[i], a[i]);
+        maxx = max(maxx, dp[i]);
+    }
+    // 环上最小子段和
+    for (int i = 1; i <= n; i++)
+    {
+        dp[i] = max(dp[i-1]+b[i], b[i]);
+        minx = max(minx, dp[i]);
+    }
+    ans = max(maxx, sum+minx);
+    if (ans == 0) cout << maxx;
+    else cout << ans;
+    return 0;
+}
